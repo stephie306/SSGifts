c1157bc04335771a74e514ff9e7b8cd6ff609a24
Stefan Bosev <stefan.bosev1@gmail.com>
2021-04-13 00:42:24 +0300

    Created login logic but failure on authentication

--- app.py
+++ app.py
@@ -1,14 +1,25 @@
 import os
-from flask import Flask, send_from_directory, render_template, redirect, url_for, request
+from flask import Flask, send_from_directory, render_template, redirect, url_for, request, jsonify
 from flask_socketio import SocketIO
 from flask_restful import Api, Resource, reqparse
 from flask_cors import CORS  # comment this on deployment
 from src.user import User
+from functools import wraps
+import json
 
 app = Flask(__name__, static_url_path='', static_folder='static', template_folder='static/html/')
 CORS(app)  # comment this on deployment
 api = Api(app)
 socket = SocketIO(app)
+
+def require_login(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        token = request.cookies.get('token')
+        if not token or not User.verify_token(token):
+            return redirect('/login')
+        return func(*args, **kwargs)
+    return wrapper
 
 
 @app.route("/")
@@ -18,7 +29,24 @@ def homepage():
 
 @app.route("/login", methods=['GET', 'POST'])
 def login():
-    return render_template('login.html')
+    if request.method == 'GET':
+        return render_template('login.html')
+    elif request.method == 'POST':
+        email = request.form['Email']
+        password = request.form['Password']
+        user = User.find_by_email(email)
+
+        print(user.password)
+        print(User.hash_password(password))
+        
+        if not user or not user.verify_password(password):
+            # app.logger.warn('%s is NOT logged!', username)
+            return jsonify({'token': None})
+
+        # app.logger.info('%s logged in successfully', username)
+        token = user.generate_token()
+        return jsonify({'token': token.decode('ascii')})
+        # return redirect("/")
 
 
 @app.route('/register', methods=['GET', 'POST'])
@@ -26,7 +54,6 @@ def register():
     if request.method == 'GET':
         return render_template('register.html')
     elif request.method == 'POST':
-        print(request.form)
         values = (
             None,
             request.form['First name'],
--- src/user.py
+++ src/user.py
@@ -1,14 +1,20 @@
 import hashlib
 
 from src.database import DB
+from itsdangerous import (
+        TimedJSONWebSignatureSerializer as Serializer,
+        BadSignature,
+        SignatureExpired
+        )
 
+SECRET_KEY = "YwY[IA,LWgZxxCmX8Mug;t2Do}}1?%Fd$:2zx!mKP9#52F[>IQb_I2aek!e,ktV"
 
 class User:
     def __init__(self, id, first_name, last_name, password, age, gender, email, address):
         self.id = id
         self.first_name = first_name
         self.last_name = last_name
-        self.password = hashlib.sha256(password.encode('utf-8')).hexdigest()
+        self.password = password
         self.age = age
         self.gender = gender
         self.email = email
@@ -16,7 +22,7 @@ class User:
 
     def create(self):
         with DB() as db:
-            values = (self.first_name, self.last_name, self.age, self.gender, self.email, self.password, self.address)
+            values = (self.first_name, self.last_name, self.age, self.gender, self.email, User.hash_password(self.password), self.address)
             db.execute('''
                 INSERT INTO User(user_first_name, user_last_name, age, gender, user_mail, password, address) VALUES(?, ?, ?, ?, ?, ?, ?)
                 ''', values)
@@ -26,14 +32,14 @@ class User:
     def delete(self):
         with DB() as db:
             db.execute('''
-                DELETE FROM Users WHERE user_id = ?
+                DELETE FROM User WHERE user_id = ?
                 ''', self.id)
 
     @staticmethod
     def find_by_firstname(first_name):
         with DB() as db:
             values = db.execute('''
-                SELECT * FROM Users WHERE username = ?
+                SELECT * FROM User WHERE user_first_name = ?
             ''', (first_name,)).fetchone()
             if values:
                 return User(*values)
@@ -43,8 +49,18 @@ class User:
     def find_by_lastname(last_name):
         with DB() as db:
             values = db.execute('''
-                SELECT * FROM Users WHERE username = ?
+                SELECT * FROM User WHERE user_last_name = ?
             ''', (last_name,)).fetchone()
+            if values:
+                return User(*values)
+            return None
+    
+    @staticmethod
+    def find_by_email(email):
+        with DB() as db:
+            values = db.execute('''
+                SELECT id, user_first_name, user_last_name, password, age, gender, user_mail, address FROM User WHERE user_mail = ?
+            ''', (email,)).fetchone()
             if values:
                 return User(*values)
             return None
@@ -55,3 +71,18 @@ class User:
 
     def verify_password(self, new_password):
         return self.password == User.hash_password(new_password)
+
+    def generate_token(self):
+        s = Serializer(SECRET_KEY, expires_in=1000)
+        return s.dumps({'username': self.username})
+
+    @staticmethod
+    def verify_token(token):
+        s = Serializer(SECRET_KEY)
+        try:
+            s.loads(token)
+        except SignatureExpired:
+            return False
+        except BadSignature:
+            return False
+        return True
